45ab3ea7e29657eaa467d4946be75b1ebd686823
conni <cbuschka@gmail.com>
2021-10-23 09:28:28 +0200

    Refactored entrypoint.

--- assets/docker-entrypoint.sh
+++ assets/docker-entrypoint.sh
@@ -28,30 +28,50 @@ else
   exit 1
 fi
 
-if [ "${COCKROACH_SECURITY_MODE}" != "insecure" ]; then
-  if [ ! -f "${COCKROACH_CERTS_DIR}/ca.key" ]; then
-    echo "Generating ca.key..."
-    /cockroach/cockroach cert create-ca --ca-key=${COCKROACH_CERTS_DIR}/ca.key
-  else
-    echo "ca.key already exists."
-  fi
+function printEnv() {
+  echo
+  echo "Cockroach env:"
+  env | grep COCKROACH
+}
 
-  if [ ! -f "${COCKROACH_CERTS_DIR}/node.crt" ]; then
-    echo "Generating node.crt..."
-    /cockroach/cockroach cert create-node --ca-key=${COCKROACH_CERTS_DIR}/ca.key roach1 localhost $(hostname)
-  else
-    echo "node.crt already exists."
-  fi
+function generateCerts() {
+  echo
+  if [ "${COCKROACH_SECURITY_MODE}" != "insecure" ]; then
+    if [ ! -f "${COCKROACH_CERTS_DIR}/ca.key" ]; then
+      echo "Generating ca.key..."
+      /cockroach/cockroach cert create-ca --ca-key=${COCKROACH_CERTS_DIR}/ca.key
+    else
+      echo "ca.key already exists."
+    fi
 
-  if [ ! -f "${COCKROACH_CERTS_DIR}/client.root.crt" ]; then
-    echo "Generating root client certificate..."
-    /cockroach/cockroach cert create-client root --ca-key=${COCKROACH_CERTS_DIR}/ca.key
-  else
-    echo "client.root.crt already exists."
-  fi
-fi
+    if [ ! -f "${COCKROACH_CERTS_DIR}/node.crt" ]; then
+      echo "Generating node.crt..."
+      /cockroach/cockroach cert create-node --ca-key=${COCKROACH_CERTS_DIR}/ca.key roach1 localhost $(hostname)
+    else
+      echo "node.crt already exists."
+    fi
 
-if [ $# -eq 0 ]; then
+    if [ ! -f "${COCKROACH_CERTS_DIR}/client.root.crt" ]; then
+      echo "Generating root client certificate..."
+      /cockroach/cockroach cert create-client root --ca-key=${COCKROACH_CERTS_DIR}/ca.key
+    else
+      echo "client.root.crt already exists."
+    fi
+  fi
+}
+
+function waitForCockroachStartup() {
+  echo
+  echo "Waiting for cockroach to come up..."
+  while ! /cockroach/cockroach sql ${COCKROACH_SECURITY_OPTS} --database defaultdb --user root -e "select 1" 1>/dev/null 2>&1; do
+    sleep 1
+  done
+  echo "Good, cockroach is reachable."
+}
+
+function startCockroach() {
+  echo
+  echo "Starting cockroach..."
   /cockroach/cockroach start-single-node \
     ${COCKROACH_SECURITY_OPTS} ${COCKROACH_SECURITY_START_OPTS} \
     --pid-file=/run/cockroach.pid \
@@ -59,35 +79,43 @@ if [ $# -eq 0 ]; then
     --background ${COCKROACH_EXTRA_START_OPTS}
   COCKROACH_PID=$(cat /run/cockroach.pid)
   echo "PID is ${COCKROACH_PID}."
+}
 
-  echo "Waiting for cockroach to come up..."
-  while ! /cockroach/cockroach sql ${COCKROACH_SECURITY_OPTS} --database defaultdb --user root -e "select 1" 1>/dev/null 2>&1; do
-    sleep 1
-  done
-  echo "Good, cockroach is reachable."
-
+function changeRootPassword() {
   if [ ! -z "${COCKROACH_ROOT_PASSWORD}" ]; then
+    echo
     echo "Changing password of root user..."
     echo "alter user root with password '${COCKROACH_ROOT_PASSWORD}';" | /cockroach/cockroach sql ${COCKROACH_SECURITY_OPTS} --database defaultdb --user root
   fi
   echo "grant admin to root;" | /cockroach/cockroach sql ${COCKROACH_SECURITY_OPTS} --database defaultdb --user root
+}
 
+function createUser() {
   if [ ! -z "${COCKROACH_USER}" ] && [ ! -z "${COCKROACH_PASSWORD}" ] ; then
     if [ -z "${COCKROACH_PASSWORD}" ]; then
+      echo
       echo "Creating user ${COCKROACH_USER} without password..."
       echo "create user ${COCKROACH_USER};" | /cockroach/cockroach sql ${COCKROACH_SECURITY_OPTS} --database defaultdb --user root
     else
+      echo
       echo "Creating user ${COCKROACH_USER} with password..."
       echo "create user ${COCKROACH_USER} with password '${COCKROACH_PASSWORD}';" | /cockroach/cockroach sql ${COCKROACH_SECURITY_OPTS} --database defaultdb --user root
     fi
     echo "grant admin to ${COCKROACH_USER};" | /cockroach/cockroach sql ${COCKROACH_SECURITY_OPTS} --database defaultdb --user root
   fi
+}
 
+function createDatabase() {
   if [ ! -z "${COCKROACH_DATABASE}" ] ; then
+    echo
     echo "Creating database ${COCKROACH_DATABASE}..."
     echo "create database if not exists ${COCKROACH_DATABASE} encoding='utf-8';" | /cockroach/cockroach sql ${COCKROACH_SECURITY_OPTS} --database defaultdb --user root
   fi
+}
 
+function migrateDatabase() {
+  echo
+  echo "Migrating database..."
   STAT_DIR=/cockroach/cockroach-data/initdb.status
   mkdir -p ${STAT_DIR}
   for SCRIPT in /docker-entrypoint-initdb.d/*; do
@@ -102,17 +130,33 @@ if [ $# -eq 0 ]; then
       touch ${STAT_FILE}
     elif [[ ! -f ${STAT_FILE} ]] && [[ "${SCRIPT}" =~ .+\.sql ]]; then
       echo -e "\nRunning ${SCRIPT}..."
-      /cockroach/cockroach sql --echo-sql ${COCKROACH_SECURITY_OPTS} --database ${COCKROACH_DATABASE:-defaultdb} --user ${COCKROACH_USER:-root} <${SCRIPT}
+      /cockroach/cockroach sql ${COCKROACH_SECURITY_OPTS} --database ${COCKROACH_DATABASE:-defaultdb} --user ${COCKROACH_USER:-root} <${SCRIPT}
       touch ${STAT_FILE}
     else
       echo -e "\nSkipping ${SCRIPT}."
     fi
   done
   echo
+}
 
-  echo "Cockroach is READY."
-
+function waitForCockroachShuttingDown() {
   tail --pid=${COCKROACH_PID} -f /dev/null
+}
+
+printEnv
+generateCerts
+
+if [ $# -eq 0 ]; then
+  startCockroach
+  waitForCockroachStartup
+  changeRootPassword
+  createUser
+  createDatabase
+  migrateDatabase
+
+  echo
+  echo "Cockroach is READY."
+  waitForCockroachShuttingDown
 elif [ "${1-}" = "shell" ]; then
   shift
   exec /bin/sh "$@"
